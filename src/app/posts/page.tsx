119e568b3bead6301f33f0589aefc88c16b3218c
Mfernna <mnfmoreir@gmail.com>
2026-01-21 16:11:38 -0300

    feat: installed axios and swr + re-organized folders

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function PostsPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Listado de Posts</h1>
+      <p>Aquí es donde usaremos SWR para mostrar la API.</p>
+    </main>
+  );
+}
